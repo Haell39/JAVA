d31962e10bca9288eae98bd8f4256d46971703ba
Rael <santosrafaelpro@gmail.com>
2024-05-26 00:36:57 -0300

    adapting java

---
+++ JAVOU/src/tuto.java
@@ -0,0 +1,11 @@
+public class tuto {
+
+    public static void main(String[] args) {
+        System.out.println("Vamos somar 2 numeros!");
+
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+}
+}
