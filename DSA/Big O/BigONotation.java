952bd30567560f985fd6576e23f4aa8d0234b49f
Rael <santosrafaelpro@gmail.com>
2024-07-14 20:31:12 -0300

    updated java

---
+++ DSA/Big O/BigONotation.java
@@ -0,0 +1,11 @@
+public class BigONotation {
+
+    public static void printFirstItem(int[] array) {
+        System.out.println(array[0]);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        printFirstItem(numbers);  // Output: 10
+    }
+}
