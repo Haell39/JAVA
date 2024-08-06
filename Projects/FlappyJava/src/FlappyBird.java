846adedb565e6ae927e0e4a543dc0def59bb055e
Rael <santosrafaelpro@gmail.com>
2024-08-05 23:56:46 -0300

    javprojectupdated

--- Projects/FlappyJava/src/FlappyBird.java
+++ Projects/FlappyJava/src/FlappyBird.java
@@ -4,52 +4,102 @@ import javax.swing.*;
 import java.util.Random;
 import java.util.ArrayList;
 
-
-
-public class FlappyBird extends JPanel {
+public class FlappyBird extends JPanel implements ActionListener {
 
     int boardwidth = 360;
     int boardheight = 640;
 
-    //Images
-
+    // Images
     Image background;
     Image birdImage;
     Image topPipeImg;
     Image bottomPipeImg;
 
+    // Bird
+    int birdX = boardwidth / 8;
+    int birdY = boardheight / 2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird {
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img) {
+            this.img = img;
+        }
+    }
+
+    // game logic
+    Bird bird;
+    int velocityY = -6;
+
+
+
+
+    Timer gameloop;
 
     FlappyBird() {
         setPreferredSize(new Dimension(boardwidth, boardheight));
-        //setBackground(Color.blue);
+        // setBackground(Color.blue);
 
-
-        //load images
-
+        // Load images
         background = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
         birdImage = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImg = new ImageIcon(getClass().getResource("bottomPipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottomPipe.png")).getImage();
 
 
+        //bird
+        bird = new Bird(birdImage);
+
+        //game timer
+
+        gameloop = new Timer(1000/60, this);
+
+        gameloop.start();
 
     }
-
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         draw(g);
     }
 
-
-
     public void draw(Graphics g) {
-        //draw background
+        // Draw background
         g.drawImage(background, 0, 0, boardwidth, boardheight, null);
+
+        // Draw bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        FlappyBird game = new FlappyBird();
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+
+    public void move() {
+        // Move bird
+        bird.y += velocityY;
+        bird.y = Math.max(bird.y, 0);
 
     }
 
-    
-    
-}
 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        move();
+        repaint();
+    }
+}
