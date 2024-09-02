dcb24dc9dd32577b42984e84338954662d4b8970
Rael <santosrafaelpro@gmail.com>
2024-09-02 20:38:39 -0300

    Update exercicio4.java

--- Java Classes College/exercicio4.java
+++ Java Classes College/exercicio4.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+//Atenção: Essa é outra forma de fazer o exercicio 04:
+/*
+ Fiz primeiro esse exercicio em python com umas 5 linhas, mas não tinha ferramentas suficiente para fazer um codigo tão otimizado no java
+ Então o Exercicio_04 foi feito de maneira bruta da forma que eu consegui fazer, e esse aqui (Exercicio04_02), Fiz com auxilio de IA até consegui otimizar o codigo ao máximo.
+*/
+
 public class exercicio4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
