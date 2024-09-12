5de0e2ac648db794258e882708df00334070c9dd
Rael <santosrafaelpro@gmail.com>
2024-09-12 17:28:01 -0300

    update

---
+++ College/Exercixes_list/Q32.java
@@ -0,0 +1,14 @@
+package College.Exercixes_list;
+import java.util.Scanner;
+
+public class Q32 {
+    //Programa que calcula a soma dos quadrados dos 20 primeiros números ímpares a partir de um número informado pelo usuário
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    
+}
+
+
